69248996b2918b6dcf858168df859a5687a805f2
Onkein <168933496+Onkein@users.noreply.github.com>
2025-02-20 16:52:46 -0600

    Interfaz
    Actualización de interfaz y cambios en AppShell.xaml

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+using System.Net.Http.Json;
 namespace TeraPollos.Views;
 
 public partial class LoginPage : ContentPage
@@ -5,5 +7,26 @@ public partial class LoginPage : ContentPage
 	public LoginPage()
 	{
 		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		using (HttpClient client = new HttpClient())
+		{
+			string urlrecurso = "http://192.168.71.208:8000/login";
+			LoginRequest datos = new LoginRequest(Convert.ToInt32(codigo.Text), contra.Text);
+			HttpResponseMessage respuesta = await client.PostAsJsonAsync(urlrecurso, datos);
+			Console.WriteLine(respuesta.Content.ToString());
+		}
+    }
+}
+class LoginRequest
+{
+	public int UserId { get; set; }
+	public string Password { get; set; }
+	public LoginRequest(int userId, string password)
+	{
+		UserId = userId;
+		Password = password;
 	}
 }
